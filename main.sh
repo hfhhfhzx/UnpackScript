abf11b9aca7bbe1eb02f5edd673e9e47aecd73ec
hfhhfhzx <159223248+hfhhfhzx@users.noreply.github.com>
2025-07-25 16:11:37 +0800

    大幅度精简

--- main.sh
+++ main.sh
@@ -1,11 +1,6 @@
-echo "请准备boot镜像与libmagiskboot.so，并将它们放在脚本所在的目录中"
-echo "libmagiskboot.so文件名不要修改。boot镜像请重命名为boot.img"
-echo " "
 path=$(dirname "$(realpath "$0")")
 
 user=$(whoami)
-echo "当前用户为$user"
-echo " "
 
 case "$user" in
     "shell")
@@ -14,34 +9,15 @@ case "$user" in
         cd /data/local/tmp
         chmod +x boot.so
         ./boot.so unpack boot.img
-        if [ -f "$path/kernel" ]; then
-        echo "回复y覆盖之前的kernel,回复N不覆盖"
-        fi
-        cp -i kernel "$path"
+        cp kernel "$path" || exit 1
         rm kernel boot.so boot.img
-        exit
         ;;
     "root")
         cd "$path"
         chmod +x libmagiskboot.so
-        if [ -f "kernel" ]; then
-        echo "回复y覆盖之前的kernel，回复其他不覆盖。"
-        read -r answer
-        answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
-        if [ "$answer" = "y" ]; then
-            ./libmagiskboot.so unpack boot.img
-        fi
-    else
         ./libmagiskboot.so unpack boot.img
-    fi
-        exit
-        ;;
-    "system")
-        echo "当前不支持该用户"
-        exit
         ;;
     *)
-        echo "该用户权限不足"
-        exit
+        echo "不支持/权限不足"
         ;;
 esac
