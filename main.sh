e4a25999842a64a32720b599720f096492b73fc6
hfhhfhzx <159223248+hfhhfhzx@users.noreply.github.com>
2025-07-25 18:28:22 +0800

    再次大幅度精简

--- main.sh
+++ main.sh
@@ -1,23 +1,8 @@
 path=$(dirname "$(realpath "$0")")
-
-user=$(whoami)
-
-case "$user" in
-    "shell")
-        cp "$path/boot.img" /data/local/tmp
+        cp "$path/boot.img" /data/local/tmp || echo "权限不足或不支持" ; exit 1
         cp "$path/libmagiskboot.so" /data/local/tmp/boot.so
         cd /data/local/tmp
         chmod +x boot.so
         ./boot.so unpack boot.img
         cp kernel "$path" || exit 1
         rm kernel boot.so boot.img
-        ;;
-    "root")
-        cd "$path"
-        chmod +x libmagiskboot.so
-        ./libmagiskboot.so unpack boot.img
-        ;;
-    *)
-        echo "不支持/权限不足"
-        ;;
-esac
