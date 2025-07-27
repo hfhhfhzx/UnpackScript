e4d7bc3d1491342e66f77ddc90d4530e6e3c1079
hfhhfhzx <159223248+hfhhfhzx@users.noreply.github.com>
2025-07-27 21:09:52 +0800

    Delete main.sh

--- main.sh
+++
@@ -1,8 +0,0 @@
-path=$(dirname "$(realpath "$0")")
-        cp "$path/boot.img" /data/local/tmp || echo "权限不足或不支持" ; exit 1
-        cp "$path/libmagiskboot.so" /data/local/tmp/boot.so
-        cd /data/local/tmp
-        chmod +x boot.so
-        ./boot.so unpack boot.img
-        cp kernel "$path" || exit 1
-        rm kernel boot.so boot.img
