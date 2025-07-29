2ee0beff9e07fd9b6af788abcfa36bb353d026d3
hfhhfhzx <159223248+hfhhfhzx@users.noreply.github.com>
2025-07-29 16:10:35 +0800

    Create 6.sh

---
+++ Script/6.sh
@@ -0,0 +1,15 @@
+path=$(dirname "$(realpath "$0")")
+        cd "$path"
+        if [ -f "kernel" ]; then
+    echo "发现kernel文件，回复y覆盖，直接回车不覆盖"
+    read -r answer
+    case "$answer" in
+        [yY])
+            ./.libmagiskboot.so unpack boot.img
+            ;;
+        *)
+            ;;
+    esac
+else
+    ./libmagiskboot.so unpack boot.img
+fi
